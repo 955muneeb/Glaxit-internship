1be55b04993eba44336aa9830e1dd21b22e1b4a1
55muneeb <awanzada1289@gmail.com>
2026-07-16 14:11:13 +0500

    this is  week 3days 1 task to undrstand the conccept of filter mpa and etc

---
+++ week-3/users.js
@@ -0,0 +1,42 @@
+// users.js
+
+const users = [
+  {
+    id: 1,
+    name: "Ali",
+    role: "Admin",
+    active: true,
+  },
+  {
+    id: 2,
+    name: "Sara",
+    role: "Teacher",
+    active: true,
+  },
+  {
+    id: 3,
+    name: "Ahmed",
+    role: "Student",
+    active: false,
+  },
+  {
+    id: 4,
+    name: "Fatima",
+    role: "Admin",
+    active: true,
+  },
+  {
+    id: 5,
+    name: "Usman",
+    role: "Teacher",
+    active: false,
+  },
+  {
+    id: 6,
+    name: "Ayesha",
+    role: "Student",
+    active: true,
+  },
+];
+
+export default users;
