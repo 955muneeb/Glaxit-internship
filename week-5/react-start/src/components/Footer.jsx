8d74e53d3c4be7963142e60f81909b893cd7e169
55muneeb <awanzada1289@gmail.com>
2026-07-29 14:31:05 +0500

    Add Footer, Header, and MainContent components

---
+++ week-5/react-start/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/Footer.jsx
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <footer style={{ padding: '10px', background: '#f1f1f1', textAlign: 'center', position: 'fixed', width: '100%', bottom: 0 }}>
+      <p>{props.copyright}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ week-5/react-start/src/components/Header.jsx
@@ -0,0 +1,13 @@
+// src/Header.jsx
+import React from 'react';
+
+function Header(props) {
+  return (
+    <header style={{ padding: '20px', background: '#282c34', color: 'white', textAlign: 'center' }}>
+      <h1>{props.title}</h1>
+      <p>{props.subtitle}</p>
+    </header>
+  );
+}
+
+export default Header;
